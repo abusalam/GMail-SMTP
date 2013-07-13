0729190eb8da6b4bfde4b2ff995374f53f9274d8
abusalam <abusalamparvezalam@gmail.com>
2013-07-13 12:58:23 +0530

    Send Mail using GMail SMTP works

---
+++ GMail.lib.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once 'config.gmail.php';
+
+function GMailSMTP($AppEMail, $AppName, $Subject, $Body) {
+  /**
+   * PHPMailer Object Instance
+   */
+  $eMail = new PHPMailer();
+  $eMail->IsSMTP();
+
+  /**
+   * Enable SMTP debugging
+   *
+   * 0 = off (for production use)
+   * 1 = client messages
+   * 2 = client and server messages
+   */
+  $eMail->SMTPDebug = 0;
+  $eMail->Debugoutput = 'html';             //Ask for HTML-friendly debug output
+
+  $eMail->Host = "smtp.gmail.com";          // sets GMAIL as the SMTP server
+  $eMail->Port = 587;                       // set the SMTP port
+  $eMail->SMTPSecure = "tls";               // sets the prefix to the server
+  $eMail->SMTPAuth = true;                  // enable SMTP authentication
+
+  $eMail->Username = GMail_UserID;          // GMAIL username
+  $eMail->Password = GMail_Pass;            // GMAIL password
+
+  $eMail->SetFrom(GMail_UserID, UserName);  //Set who the message is to be sent from
+  $eMail->AddReplyTo(GMail_UserID, UserName); //Set an alternative reply-to address
+  $eMail->AddAddress($AppEMail, $AppName);  //Set who the message is to be sent to
+
+  $eMail->Subject = $Subject;
+  $eMail->MsgHTML($Body);
+  $eMail->AltBody = "View this page in HTML"; //Text Body
+  $eMail->WordWrap = 50;                    // set word wrap
+  //$eMail->AddAttachment("/path/to/file.zip");             // attachment
+  //$eMail->AddAttachment("/path/to/image.jpg", "new.jpg"); // attachment
+
+  $eMail->IsHTML(true); // send as HTML
+  if (!$eMail->Send()) {
+    $Resp['Status'] = "Mailer Error: " . $eMail->ErrorInfo;
+    $Resp['Sent'] = FALSE;
+  } else {
+    $Resp['Status'] = "Message has been sent";
+    $Resp['Sent'] = TRUE;
+  }
+  $Resp['IP'] = "Requested From:" . $_SERVER['REMOTE_ADDR'];
+  return json_encode($Resp);
+}
+
+if ($_SERVER["REMOTE_ADDR"] === AllowedIP) {
+  echo GMailSMTP(GetVal($_REQUEST, 'AppEmail'), GetVal($_REQUEST, 'AppName'), GetVal($_REQUEST, 'Subject'), GetVal($_REQUEST, 'Body'));
+}
+?>
