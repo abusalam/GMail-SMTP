d5d0af9bdbcfba660364cb8aaf18785a41eafeb8
abusalam <abusalamparvezalam@gmail.com>
2013-08-21 22:52:12 +0530

    Fixed lib.inc.php include issue

--- config.gmail.php
+++ config.gmail.php
@@ -5,7 +5,6 @@ define("GMail_UserID", "username@gmail.com");
 define("GMail_Pass", "password");
 define("UserName", "Paschim Medinipur");
 define("AllowedIP", "127.0.0.0");
-include_once '../lib.inc.php';
 include_once 'class.phpmailer.php';
 //SMTP needs accurate times, and the PHP time zone MUST be set
 //This should be done in your php.ini, but this is how to do it if you don't have access to that
